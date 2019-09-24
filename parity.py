80cd58f5dcc198ca978bc13861925b95707d1468
Bill Mahoney <bill.mahoney@microchip.com>
2019-09-23 20:08:47 -0700

    changes

--- parity.py
+++ parity.py
@@ -1,11 +1,5 @@
 import sys
 import math
-# def parity(x):
-#     result = 0
-#     while x:
-#         result ^= x & 1
-#         x >>= 1
-#     return result
 
 def parity(x):
     n = len(bin(x))
@@ -18,12 +12,11 @@ def parity(x):
             print ("O({})".format(count))
             return x & 1
         if n%2 != 0:
-            x = (((x >> 1) ^ x) & 1) | ((x >> 1)& ~(1 << 0))
+            x = (((x >> 1) ^ x) & 1) | ((x >> 1) & ~(1 << 0))
             n -= 1
         else:
             n = int(n/2)
             x ^= (x >> n)
-        # n = int(n/2)
         
     return x & 1
 
